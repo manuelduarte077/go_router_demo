4977ccf5157279a260d949c4c4e9342d0272e632
manuelduarte077 <carlosd99m@gmail.com>
2022-09-19 13:33:09 -0600

    chore(packages): add device preview

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,11 +1,16 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router_demo/routes/router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
         elevatedButtonTheme: ElevatedButtonThemeData(
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router_demo/app/app.dart';
 
+import 'package:device_preview/device_preview.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const MyApp(),
+  ));
 }
