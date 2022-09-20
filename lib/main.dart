71a46ca2cec8f7b177a61ec8359423f6878d9c35
manuelduarte077 <carlosd99m@gmail.com>
2022-09-19 22:11:11 -0600

    chore(packages): add SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:go_router_demo/app/app.dart';
 
 import 'package:device_preview/device_preview.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => const MyApp(),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// SharedPreferences
+  final SharedPreferences sharedPreferences =
+      await SharedPreferences.getInstance();
+
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(sharedPreferences: sharedPreferences),
+    ),
+  );
 }
